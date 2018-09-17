0f7ad942eab328caa6a6d65b40e61c4fc33ed54d
konradhalas <halas.konrad@gmail.com>
2018-09-17 09:34:56 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='buslane',
-    version='0.0.1',
+    version='0.0.2',
     python_requires='>=3.6',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
