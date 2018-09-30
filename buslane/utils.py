7d0744337d15d0e3dbea63dc16907856ce323d5f
konradhalas <halas.konrad@gmail.com>
2018-09-30 18:51:57 +0200

    Fix Python 3.7

--- buslane/utils.py
+++ buslane/utils.py
@@ -12,6 +12,9 @@ def get_message_cls(handler_cls: Type, base_message_cls: Type[T]) -> Type[T]:
         for base in handler_cls.__orig_bases__:
             if base.__args__:
                 for arg in base.__args__:
-                    if issubclass(arg, base_message_cls):
-                        return arg
+                    try:
+                        if issubclass(arg, base_message_cls):
+                            return arg
+                    except TypeError:
+                        pass
     raise WrongHandlerException()
